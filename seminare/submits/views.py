af80abdd37b54d91b615674dc0d005131c33733b
Filipko^^ <fiilgameslp@gmail.com>
2024-12-08 13:57:12 +0100

    add submit list and details

--- seminare/submits/views.py
+++ seminare/submits/views.py
@@ -1,1 +1,38 @@
 # Create your views here.
+from django.views.generic import DetailView, TemplateView
+
+from seminare.submits.models import FileSubmit, JudgeSubmit, TextSubmit
+
+
+class SubmitListView(TemplateView):
+    template_name = "submits/list.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        user = self.request.user
+        problem_id = self.kwargs.get("problem_id")
+        fs = FileSubmit.objects.filter(enrollment__user=user, problem=problem_id).all()
+        js = JudgeSubmit.objects.filter(enrollment__user=user, problem=problem_id).all()
+        ts = TextSubmit.objects.filter(enrollment__user=user, problem=problem_id).all()
+        context = super().get_context_data()
+        context["file_submits"] = fs
+        context["judge_submits"] = js
+        context["text_submits"] = ts
+        return context
+
+
+class FileSubmitDetailView(DetailView):
+    model = FileSubmit
+    template_name = "submits/detail_file.html"
+    context_object_name = "submit"
+
+
+class JudgeSubmitDetailView(DetailView):
+    model = JudgeSubmit
+    template_name = "submits/detail_judge.html"
+    context_object_name = "submit"
+
+
+class TextSubmitDetailView(DetailView):
+    model = TextSubmit
+    template_name = "submits/detail_text.html"
+    context_object_name = "submit"
--- seminare/urls.py
+++ seminare/urls.py
@@ -1,12 +1,20 @@
 import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.shortcuts import render
 from django.urls import include, path
+
 from seminare.problems import urls as problems_urls
+from seminare.submits import urls as submits_urls
 
 urlpatterns = [
     path("problems/", include(problems_urls)),
     path("admin/", admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),
     path("test/", lambda request: render(request, "test.html")),
+    path("submits/", include(submits_urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
