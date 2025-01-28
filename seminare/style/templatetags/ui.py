9f95e479337513308a57185b67544506f30eeec3
Adam Zahradník <adam@zahradnik.xyz>
2025-01-28 20:41:48 +0100

    add breadcrumbs to problem detail

--- seminare/style/templatetags/ui.py
+++ seminare/style/templatetags/ui.py
@@ -6,3 +6,12 @@ register = template.Library()
 @register.inclusion_tag("_ui/label.html")
 def label(message, color="gray", *, icon="", help=""):
     return {"message": message, "color": color, "icon": icon, "help": help}
+
+
+@register.inclusion_tag("_ui/breadcrumbs.html")
+def breadcrumbs(*args):
+    crumbs = []
+    for i in range(0, len(args), 2):
+        crumbs.append(args[i : i + 2])
+
+    return {"crumbs": crumbs}
