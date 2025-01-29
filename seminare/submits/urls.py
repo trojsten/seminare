c63cc42ee8a3cdab9a7c9c55e82c4126e6272f93
Adam Zahradník <adam@zahradnik.xyz>
2025-01-29 12:08:32 +0100

    fix: problem and submit breadcrumbs

--- seminare/submits/urls.py
+++ seminare/submits/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path(
         "submits/<submit_id:submit_id>/",
         SubmitDetailView.as_view(),
-        name="file_submit_detail",
+        name="submit_detail",
     ),
     path(
         "file_submit/create/<int:problem>", file_submit_create_view, name="file_submit"
