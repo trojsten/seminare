7b905efb4cdaa05c7d9a1b126489bd43dc0902e8
Adam Zahradník <adam@zahradnik.xyz>
2025-01-29 23:19:38 +0100

    fix sidebar without contest

--- seminare/organizer/templatetags/org_navbar.py
+++ seminare/organizer/templatetags/org_navbar.py
@@ -1,5 +1,9 @@
 from django import template
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.exceptions import MultipleObjectsReturned
 from django.urls import reverse
+
+from seminare.contests.models import Contest
 
 register = template.Library()
 
@@ -8,18 +12,42 @@ register = template.Library()
 def org_navbar(context):
     sections = []
 
-    sections.append(
-        (
-            "Section",
-            [
-                ("mdi:home", "Test", "#"),
-                (
-                    "mdi:abacus",
-                    "Sady úloh",
-                    reverse("org:problemset_list", args=[context.get("contest").id]),
-                ),
-            ],
+    contest_id = None
+    if hasattr(context["request"], "resolver_match"):
+        contest_id = context["request"].resolver_match.kwargs.get("contest_id")
+
+    site = get_current_site(context["request"])
+    if not contest_id:
+        try:
+            contest = Contest.objects.get(site=site)
+        except (Contest.DoesNotExist, MultipleObjectsReturned):
+            contest = None
+    else:
+        contest = Contest.objects.filter(id=contest_id, site=site).first()
+
+    if contest:
+        sections.append(
+            (
+                "Zadania",
+                [
+                    (
+                        "mdi:abacus",
+                        "Sady úloh",
+                        reverse("org:problemset_list", args=[contest.id]),
+                    ),
+                ],
+            )
         )
-    )
+
+    content_section = [
+        (
+            "mdi:file-document",
+            "Stránky",
+            reverse("org:page_list"),
+        )
+    ]
+
+    if content_section:
+        sections.append(("Obsah", content_section))
 
     return {"menu_sections": sections}
