dbbda8de69ac6ebf214cb927cc57c03e951ef362
Aiq0 <66842415+Aiq0@users.noreply.github.com>
2025-07-23 11:09:59 +0200

    feat(rules): add options parsing

--- seminare/rules/ksp.py
+++ seminare/rules/ksp.py
@@ -1,10 +1,26 @@
 from django.utils import timezone
+from django.utils.dateparse import parse_datetime
 
 from seminare.problems.models import Problem, Text
 from seminare.rules import RuleEngine
 
 
 class KSPRules(RuleEngine):
+    def parse_options(self, options: dict) -> None:
+        super().parse_options(options)
+
+        if "doprogramovanie_date" not in options:
+            raise ValueError("Chýba 'doprogramovanie_date'.")
+
+        self.doprogramovanie_date = parse_datetime(
+            options.get("doprogramovanie_date", None)
+        )
+
+        if not self.doprogramovanie_date:
+            raise ValueError("'doprogramovanie_date' je v neplatnom formáte.")
+
+        return
+
     def get_visible_texts(self, problem: "Problem") -> "set[Text.Type]":
         visible = set()
         now = timezone.now()
