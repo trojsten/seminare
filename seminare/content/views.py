7a21e44a219f55ce22a615cd5d99173833d826f2
Adam Zahradník <adam@zahradnik.xyz>
2025-08-05 19:48:44 +0200

    feat: add edit button to page detail (#42)
    Co-authored-by: Andrej Lackovič <andrej.lackovic@trojsten.sk>

--- seminare/content/views.py
+++ seminare/content/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
 
 from seminare.content.models import Page, Post
+from seminare.users.logic.permissions import has_contest_role
+from seminare.users.models import ContestRole, User
 
 
 class PageDetailView(DetailView):
@@ -11,6 +13,17 @@ class PageDetailView(DetailView):
     def get_object(self, queryset=...):
         site = get_current_site(self.request)
         return get_object_or_404(Page, slug=self.kwargs["slug"], contest__site=site)
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        if self.request.user.is_authenticated:
+            assert isinstance(self.request.user, User)
+            ctx["is_organizer"] = has_contest_role(
+                self.request.user, self.object.contest, ContestRole.Role.ORGANIZER
+            )
+        else:
+            ctx["is_organizer"] = False
+        return ctx
 
 
 class PostListView(ListView):
