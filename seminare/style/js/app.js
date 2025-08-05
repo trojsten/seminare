0cb50389d1b3615b588f5382f32826ef3c77e2d4
Aiq0 <66842415+Aiq0@users.noreply.github.com>
2025-08-05 15:48:45 +0200

    feat: add `scroll-to-me` class

--- seminare/style/js/app.js
+++ seminare/style/js/app.js
@@ -1,12 +1,15 @@
-import 'iconify-icon'
-import htmx from 'htmx.org'
-import tippy from "tippy.js"
 import { Application } from "@hotwired/stimulus"
+import htmx from 'htmx.org'
+import 'iconify-icon'
 import { definitions } from 'stimulus:./controllers'
+import tippy from "tippy.js"
 
 window.htmx = htmx
 htmx.on("htmx:load", () => {
     tippy('[data-tippy-content]')
+    for (const el of document.getElementsByClassName("scroll-to-me")) {
+        el.scrollIntoView({ behavior: "smooth", block: "center" })
+    }
 })
 
 const app = Application.start()
