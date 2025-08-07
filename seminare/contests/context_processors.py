4b9d06959257232a9ea041916a037eb8205a48f7
Aiq0 <66842415+Aiq0@users.noreply.github.com>
2025-08-07 10:45:16 +0200

    perf: avoid additional query by using cached contest

--- seminare/contests/context_processors.py
+++ seminare/contests/context_processors.py
@@ -1,12 +1,7 @@
-from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpRequest
 
-from seminare.contests.models import Contest
+from seminare.contests.utils import get_current_contest
 
 
 def contest(request: HttpRequest) -> dict:
-    site = get_current_site(request)
-
-    contest = Contest.objects.get(site=site)
-
-    return {"contest": contest}
+    return {"contest": get_current_contest(request)}
