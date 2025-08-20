e6bb29047b267058164a33f661934a59e31f1c67
Aiq0 <66842415+Aiq0@users.noreply.github.com>
2025-08-20 16:28:51 +0200

    feat: make results serializable

--- seminare/rules/results.py
+++ seminare/rules/results.py
@@ -1,19 +1,35 @@
 from dataclasses import dataclass
 from decimal import Decimal
+from typing import Self
 
-from seminare.rules.scores import Score
-from seminare.users.models import Enrollment
+from seminare.rules.scores import ResultsSerializable, Score
+from seminare.users.models import Enrollment, School, User
 
 
 @dataclass
-class ColumnHeader:
+class ColumnHeader(ResultsSerializable):
     title: str
     link: str | None
     tooltip: str | None
 
+    def serialize(self) -> dict:
+        return {
+            "title": self.title,
+            "link": self.link,
+            "tooltip": self.tooltip,
+        }
+
+    @classmethod
+    def deserialize(cls, data: dict) -> Self:
+        return cls(
+            title=data["title"],
+            link=data.get("link"),
+            tooltip=data.get("tooltip"),
+        )
+
 
 @dataclass
-class Cell:
+class Cell(ResultsSerializable):
     @property
     def display_cell(self) -> str:
         raise NotImplementedError()
@@ -25,6 +41,40 @@ class Cell:
     @property
     def ghost(self) -> bool:
         return False
+
+    def serialize(self) -> dict:
+        return {
+            "display_cell": self.display_cell,
+            "display_tooltip": self.display_tooltip,
+            "ghost": self.ghost,
+        }
+
+    @classmethod
+    def deserialize(cls, data: dict):
+        return FrozenCell(
+            _cell=data["display_cell"],
+            _tooltip=data.get("display_tooltip"),
+            _ghost=data.get("ghost", False),
+        )
+
+
+@dataclass
+class FrozenCell(Cell):
+    _cell: str
+    _tooltip: str | None
+    _ghost: bool
+
+    @property
+    def display_cell(self) -> str:
+        return self._cell
+
+    @property
+    def display_tooltip(self) -> str | None:
+        return self._tooltip
+
+    @property
+    def ghost(self) -> bool:
+        return self._ghost
 
 
 @dataclass
@@ -91,7 +141,7 @@ class Row:
 
 
 @dataclass
-class Table:
+class Table(ResultsSerializable):
     columns: list[ColumnHeader]
     rows: list[Row]
 
@@ -111,3 +161,89 @@ class Table:
             if not row.ghost:
                 rank += 1
                 last_total = row.total
+
+    def serialize(self) -> dict:
+        rows: list[dict] = []
+        schools = {}
+
+        for row in self.rows:
+            enrollment = row.enrollment
+            if row.enrollment.school_id not in schools:
+                schools[row.enrollment.school_id] = {
+                    "name": enrollment.school.name,
+                    "short_name": enrollment.school.short_name,
+                    "edu_id": enrollment.school.edu_id,
+                    "address": enrollment.school.address,
+                }
+
+            rows.append(
+                {
+                    "rank": row.rank,
+                    "enrollment": {
+                        "id": enrollment.id,
+                        "grade": enrollment.grade,
+                        "school_id": enrollment.school_id,
+                        "user": {
+                            "id": enrollment.user_id,
+                            "username": enrollment.user.username,
+                            "email": enrollment.user.email,
+                            "first_name": enrollment.user.first_name,
+                            "last_name": enrollment.user.last_name,
+                        },
+                    },
+                    "ghost": row.ghost,
+                    "columns": [
+                        col.serialize() if col else None for col in row.columns
+                    ],
+                    "total": str(row.total),
+                }
+            )
+
+        return {
+            "columns": [col.serialize() for col in self.columns],
+            "rows": rows,
+            "_schools": schools,
+        }
+
+    @classmethod
+    def deserialize(cls, data: dict) -> Self:
+        rows = []
+        schools = {}
+
+        for school in data["_schools"]:
+            schools[school["id"]] = School(
+                id=school["id"],
+                name=school["name"],
+                short_name=school["short_name"],
+                edu_id=school["edu_id"],
+                address=school["address"],
+            )
+
+        for row in data["rows"]:
+            rows.append(
+                Row(
+                    rank=row["rank"],
+                    enrollment=Enrollment(
+                        id=row["enrollment"]["id"],
+                        grade=row["enrollment"]["grade"],
+                        school=schools[row["enrollment"]["school_id"]],
+                        user=User(
+                            id=row["enrollment"]["user"]["id"],
+                            username=row["enrollment"]["user"]["username"],
+                            email=row["enrollment"]["user"]["email"],
+                            first_name=row["enrollment"]["user"]["first_name"],
+                            last_name=row["enrollment"]["user"]["last_name"],
+                        ),
+                    ),
+                    ghost=row.get("ghost", False),
+                    columns=[
+                        Cell.deserialize(col) if col else None for col in row["columns"]
+                    ],
+                    total=Decimal(row["total"]),
+                )
+            )
+
+        return cls(
+            columns=[ColumnHeader.deserialize(col) for col in data["columns"]],
+            rows=rows,
+        )
--- seminare/rules/scores.py
+++ seminare/rules/scores.py
@@ -1,10 +1,19 @@
 from decimal import Decimal
-from typing import Sequence
+from typing import Self, Sequence
 
 from seminare.submits.models import BaseSubmit
 
 
-class Score:
+class ResultsSerializable:
+    def serialize(self) -> dict:
+        raise NotImplementedError()
+
+    @classmethod
+    def deserialize(cls, data: dict) -> Self:
+        raise NotImplementedError()
+
+
+class Score(ResultsSerializable):
     def __init__(self, submits: Sequence[BaseSubmit]):
         self.submits = submits
 
@@ -31,3 +40,46 @@ class Score:
             display_string += "?"
 
         return display_string
+
+    def serialize(self) -> dict:
+        return {
+            "submits": [submit.id for submit in self.submits],
+            "points": str(self.points),
+            "pending": self.pending,
+            "display": self.display,
+        }
+
+    @classmethod
+    def deserialize(cls, data: dict):
+        return FrozenScore(
+            submits=[],
+            points=Decimal(data["points"]),
+            pending=data["pending"],
+            display=data["display"],
+        )
+
+
+class FrozenScore(Score):
+    def __init__(
+        self,
+        submits: Sequence[BaseSubmit],
+        points: Decimal,
+        pending: bool,
+        display: str,
+    ):
+        self._points = points
+        self._pending = pending
+        self._display = display
+        super().__init__(submits)
+
+    @property
+    def points(self) -> Decimal:
+        return self._points
+
+    @property
+    def pending(self) -> bool:
+        return self._pending
+
+    @property
+    def display(self) -> str:
+        return self._display
