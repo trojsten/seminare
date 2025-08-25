a8d4fd254a345270bf98466908d1321f3316a500
Adam Zahradník <adam@zahradnik.xyz>
2025-08-25 12:03:10 +0200

    add files to sidebar

--- seminare/organizer/templatetags/org_navbar.py
+++ seminare/organizer/templatetags/org_navbar.py
@@ -1,42 +1,33 @@
 from django import template
 from django.urls import reverse
 
-from seminare.contests.utils import get_current_contest
-
 register = template.Library()
 
 
-@register.inclusion_tag("org/_navbar.html", takes_context=True)
-def org_navbar(context):
-    sections = []
-
-    contest = get_current_contest(context["request"])
-
-    if contest:
-        sections.append(
-            (
-                "Zadania",
-                [
-                    (
-                        "mdi:abacus",
-                        "Sady úloh",
-                        reverse("org:problemset_list"),
-                    ),
-                ],
-            )
-        )
-
+@register.inclusion_tag("org/_navbar.html")
+def org_navbar():
     content_section = [
         (
             "mdi:file-document",
             "Stránky",
             reverse("org:page_list"),
-        )
+        ),
+        ("mdi:newspaper", "Príspevky", reverse("org:post_list")),
+        ("mdi:folder", "Súbory", reverse("org:file_list")),
     ]
-    if contest:
-        content_section.append(("mdi:newspaper", "Príspevky", reverse("org:post_list")))
 
-    if content_section:
-        sections.append(("Obsah", content_section))
+    sections = [
+        (
+            "Zadania",
+            [
+                (
+                    "mdi:abacus",
+                    "Sady úloh",
+                    reverse("org:problemset_list"),
+                ),
+            ],
+        ),
+        ("Obsah", content_section),
+    ]
 
     return {"menu_sections": sections}
