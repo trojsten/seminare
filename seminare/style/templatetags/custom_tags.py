42d9d3ec1c14b37f34b9e2cd00f348df9e0e5760
Andrej Lackovič <andrej.lackovic@trojsten.sk>
2025-08-29 12:22:16 +0200

    chore: clean up templates (#59)

--- seminare/style/templatetags/custom_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.filter
-def dict_key(dictionary, key):
-    """Returns the value for a given key in the dictionary."""
-    return dictionary.get(key)
