d0a5b9c6adc27a51122d0a50d96d088ad80a1143
Adam Zahradník <adam@zahradnik.xyz>
2025-09-18 22:01:39 +0200

    feat: problemset list improvements (#64)
    Co-authored-by: Andrej Lackovič <andrej.lackovic@trojsten.sk>

--- seminare/users/logic/schools.py
+++ seminare/users/logic/schools.py
@@ -1,3 +1,7 @@
+from datetime import date, datetime
+
+from django.utils import timezone
+
 from seminare.users.models import Grade
 
 DEFAULT_GRADES = {
@@ -48,3 +52,12 @@ def get_grade_from_type_year(school_type: str, year: int) -> Grade | None:
         return None
 
     return map[year]
+
+
+def date_to_academic_year(date: date | datetime) -> int:
+    if isinstance(date, datetime):
+        date = date.astimezone(timezone.get_current_timezone())
+    september = date.replace(month=9, day=1)
+    if date < september:
+        return date.year - 1
+    return date.year
