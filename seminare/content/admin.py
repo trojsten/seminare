6d746a6315773ad5704c08ce0fdea46d25d7d1b6
Aiq0 <66842415+Aiq0@users.noreply.github.com>
2026-01-30 15:10:19 +0100

    feat: better admin

--- seminare/content/admin.py
+++ seminare/content/admin.py
@@ -5,19 +5,24 @@ from seminare.content.models import MenuGroup, MenuItem, Page, Post
 
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "contest"]
     list_filter = ["contest"]
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["title", "slug", "author"]
 
 
 @admin.register(MenuGroup)
 class MenuGroupAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["title", "contest", "order"]
+    list_filter = ["contest"]
+    list_editable = ["order"]
 
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["title", "group", "order"]
+    list_filter = ["group__contest", "group"]
+    list_editable = ["order"]
