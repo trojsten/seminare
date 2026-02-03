c519601a372016d5cd12f16c483ebb02e9a64e59
Andrej Lackovič <andrej.lackovic@trojsten.sk>
2026-02-03 18:28:20 +0100

    feat: accept pushed user data from Trojsten ID (#80)

--- seminare/organizer/api/auth.py
+++ seminare/organizer/api/auth.py
@@ -1,5 +1,9 @@
 from typing import TYPE_CHECKING
 
+from django.conf import settings
+from django.contrib.auth.hashers import check_password
+from rest_framework.authentication import BaseAuthentication
+from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.permissions import BasePermission
 
 from seminare.contests.utils import get_current_contest
@@ -19,3 +23,19 @@ class IsContestAdmin(BasePermission):
         assert isinstance(request.user, User)
         contest = get_current_contest(request)
         return is_contest_administrator(request.user, contest)
+
+
+class OIDCAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        client_id = request.headers.get("X-Client-ID")
+        client_secret = request.headers.get("X-Client-Secret")
+
+        if not client_id or not client_secret:
+            raise AuthenticationFailed("Missing authentication headers")
+
+        if client_id != settings.OIDC_RP_CLIENT_ID or not check_password(
+            settings.OIDC_RP_CLIENT_SECRET, client_secret
+        ):
+            raise AuthenticationFailed()
+
+        return (None, None)
--- seminare/users/auth.py
+++ seminare/users/auth.py
@@ -1,9 +1,6 @@
-from datetime import date
-
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 
-from seminare.users.logic.schools import get_grade_from_type_year
-from seminare.users.models import School, User
+from seminare.users.models import User
 
 
 def logout_url(request):
@@ -38,31 +35,4 @@ class TrojstenOIDCAB(OIDCAuthenticationBackend):
         user.first_name = claims.get("given_name", "")
         user.last_name = claims.get("family_name", "")
 
-        self._update_school_info(user, claims.get("school_info"))
-
-    def _update_school_info(self, user, school_info):
-        if not school_info:
-            user.current_school = None
-            user.current_grade = ""
-            return
-
-        end_date = school_info["end_date"]
-        is_expired = end_date and date.fromisoformat(end_date) < date.today()
-        if is_expired:
-            user.current_school = None
-            user.current_grade = ""
-            return
-
-        school_data = school_info["school"]
-        school, _ = School.objects.get_or_create(
-            edu_id=school_data["eduid"],
-            defaults={
-                "name": school_data["name"],
-                "address": school_data["address"],
-            },
-        )
-        user.current_school = school
-
-        school_type = school_info["school_type"]
-        current_year = int(school_info["current_year"])
-        user.current_grade = get_grade_from_type_year(school_type, current_year) or ""
+        user.update_school_info(claims.get("school_info"))
