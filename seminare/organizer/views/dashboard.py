1423b19c6a1180280f0958625737d6fca090978d
Aiq0 <66842415+Aiq0@users.noreply.github.com>
2026-02-16 12:03:27 +0100

    fix: missing `ContestOrganizerRequired`

--- seminare/organizer/views/dashboard.py
+++ seminare/organizer/views/dashboard.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
 
 from seminare.organizer.views import WithContest
+from seminare.users.mixins.permissions import ContestOrganizerRequired
 
 
-class ContestDashboardView(WithContest, TemplateView):
+class ContestDashboardView(ContestOrganizerRequired, WithContest, TemplateView):
     template_name = "org/contest_dashboard.html"
