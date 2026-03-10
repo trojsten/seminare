2bf5ab995f479e993bae93f8ca45a76b199f50e2
ViktorKoL <44502667+ViktorKoL@users.noreply.github.com>
2026-03-10 23:10:24 +0100

    fix: move user section of navbar back to the end on PC (#137)
    Co-authored-by: Aiq0 <66842415+Aiq0@users.noreply.github.com>

--- seminare/content/templatetags/navbar.py
+++ seminare/content/templatetags/navbar.py
@@ -1,7 +1,7 @@
 from django import template
 from django.urls import reverse
 
-from seminare.content.models import MenuGroup, MenuItem
+from seminare.content.models import MenuItem
 from seminare.contests.utils import get_current_contest
 from seminare.users.logic.permissions import is_contest_organizer
 
@@ -11,15 +11,18 @@ register = template.Library()
 @register.inclusion_tag("navbar/menu.html", takes_context=True)
 def navbar_menu(context):
     contest = get_current_contest(context["request"])
-    items = list()
+    items = list(
+        MenuItem.objects.filter(group__contest=contest).select_related("group").all()
+    )
 
     context["items"] = items
 
     user = context["user"]
     if user.is_authenticated:
-        group = MenuGroup(id=-1, title=user.display_name)
+        user_section = list()
+        context["user_section"] = user_section
+
         logout = MenuItem(
-            group=group,
             title="Odhlásiť sa",
             icon="mdi:logout",
             url=reverse("oidc_logout"),
@@ -27,31 +30,23 @@ def navbar_menu(context):
         setattr(logout, "post", True)
 
         if is_contest_organizer(user, contest):
-            items.append(
+            user_section.append(
                 MenuItem(
-                    group=group,
                     title="Organizátorské rozhranie",
                     icon="mdi:account-tie",
                     url=reverse("org:contest_dashboard"),
                 )
             )
 
-        items.extend(
+        user_section.extend(
             [
                 MenuItem(
-                    group=group,
                     title="Môj profil",
                     icon="mdi:user",
                     url="https://id.trojsten.sk/",
                 ),
                 logout,
             ]
-        )
-
-        items.extend(
-            MenuItem.objects.filter(group__contest=contest)
-            .select_related("group")
-            .all()
         )
 
     return context
