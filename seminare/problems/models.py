0b2f839d89a4e796ae7b0aabd7319753bc5de118
Viktor <viktormg.blesk@gmail.com>
2026-03-19 13:20:55 +0100

    fix: deadline_font return type

--- seminare/problems/models.py
+++ seminare/problems/models.py
@@ -121,7 +121,7 @@ class ProblemSet(models.Model):
         return date_to_academic_year(self.start_date)
 
     @property
-    def deadline_font(self) -> int:
+    def deadline_font(self) -> str:
         now = timezone.now()
         if now > self.end_date:
             return "text-muted"
