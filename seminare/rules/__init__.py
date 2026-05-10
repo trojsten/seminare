1e69a2327964a935f76dfa70f9f177dd6438d666
Jakub Šiagi <jakub.siagi@gmail.com>
2026-05-10 21:12:11 +0200

    feat: ksp doprogramovanie (#192)
    Co-authored-by: Andrej Lackovič <andrej.lackovic@trojsten.sk>

--- seminare/rules/__init__.py
+++ seminare/rules/__init__.py
@@ -55,7 +55,7 @@ class AbstractRuleEngine:
 
     # === Contestant frontend ===
 
-    def get_important_dates(self) -> list[tuple[datetime, str]]:
+    def get_important_dates(self) -> list[tuple[datetime, str, bool]]:
         """
         Returns a list of important dates for the problem set.
         This is shown to the contestants.
@@ -291,10 +291,10 @@ class RuleEngineDataMixin:
 
 
 class RuleEngine(RuleEngineDataMixin, AbstractRuleEngine):
-    def get_important_dates(self) -> list[tuple[datetime, str]]:
+    def get_important_dates(self) -> list[tuple[datetime, str, bool]]:
         return [
-            (self.problem_set.start_date, "Začiatok kola"),
-            (self.problem_set.end_date, "Koniec kola"),
+            (self.problem_set.start_date, "Začiatok kola", False),
+            (self.problem_set.end_date, "Koniec kola", True),
         ]
 
     def get_visible_texts(self, problem: "Problem|None") -> "set[Text.Type]":
